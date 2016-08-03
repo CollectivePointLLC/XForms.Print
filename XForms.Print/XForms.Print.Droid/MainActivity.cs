30b8af979b5ed21660283cc74b14a9f9da89277d
Carson Hatter <carson@collectivepoint.com>
2016-08-03 13:58:58 -0500

    initial commit

---
+++ XForms.Print/XForms.Print.Droid/MainActivity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Xamarin.Forms.Platform.Android;
+
+namespace XForms.Print.Droid
+{
+	[Activity (Label = "XForms.Print.Droid", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : FormsApplicationActivity
+	{
+		int count = 1;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+
+            LoadApplication(new App());
+        }
+	}
+}
+
+
