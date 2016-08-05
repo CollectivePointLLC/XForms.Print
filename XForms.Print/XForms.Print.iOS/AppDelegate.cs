27cde6648b19f751268faad5fdf26523834a4c24
Carson Hatter <carson@collectivepoint.com>
2016-08-05 10:23:48 -0500

    added ios printing func

--- XForms.Print/XForms.Print.iOS/AppDelegate.cs
+++ XForms.Print/XForms.Print.iOS/AppDelegate.cs
@@ -6,8 +6,8 @@ namespace XForms.Print.iOS
 	// The UIApplicationDelegate for the application. This class is responsible for launching the
 	// User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
 	[Register ("AppDelegate")]
-	public class AppDelegate : UIApplicationDelegate
-	{
+	public class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
 		// class-level declarations
 
 		public override UIWindow Window {
@@ -17,10 +17,14 @@ namespace XForms.Print.iOS
 
 		public override bool FinishedLaunching (UIApplication application, NSDictionary launchOptions)
 		{
-			// Override point for customization after application launch.
-			// If not required for your application you can safely delete this method
-			return true;
-		}
+            // Override point for customization after application launch.
+            // If not required for your application you can safely delete this method
+            global::Xamarin.Forms.Forms.Init();
+
+            LoadApplication(new App());
+
+            return base.FinishedLaunching(application, launchOptions);
+        }
 
 		public override void OnResignActivation (UIApplication application)
 		{
---
+++ XForms.Print/XForms.Print.iOS/Services/PrinterService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UIKit;
+using Xamarin.Forms;
+using XForms.Print.iOS.Services;
+using XForms.Print.Services;
+
+[assembly: Dependency(typeof(PrinterService))]
+namespace XForms.Print.iOS.Services
+{
+    public class PrinterService : IPrinterService
+    {
+        public void Print()
+        {
+            var printInfo = UIPrintInfo.PrintInfo;
+            printInfo.OutputType = UIPrintInfoOutputType.General;
+            printInfo.JobName = "My first Print Job";
+
+            var textFormatter = new UISimpleTextPrintFormatter("Once upon a time...")
+            {
+                StartPage = 0,
+                ContentInsets = new UIEdgeInsets(72, 72, 72, 72),
+                MaximumContentWidth = 6 * 72,
+            };
+
+            var printer = UIPrintInteractionController.SharedPrintController;
+            printer.PrintInfo = printInfo;
+            printer.PrintFormatter = textFormatter;
+            printer.ShowsPageRange = true;
+            printer.Present(true, (handler, completed, err) => {
+                if (!completed && err != null)
+                {
+                    Console.WriteLine("error");
+                }
+            });
+        }
+    }
+}
